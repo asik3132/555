91320110d3112a4c8b31c5800fcea6580e7b4e47
Askar <asik9777@gmail.com>
2023-03-06 20:05:58 +0600

    first commit

---
+++ bin/home_works.dart
@@ -0,0 +1,5 @@
+import 'package:home_works/home_works.dart' as home_works;
+
+void main(List<String> arguments) {
+  print('Hello world: ${home_works.calculate()}!');
+}
---
+++ test/home_works_test.dart
@@ -0,0 +1,8 @@
+import 'package:home_works/home_works.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
